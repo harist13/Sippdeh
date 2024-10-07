d64dd4a64f2a8e93244b186e32c34daad20767ef
novilfahlevy <novilfreon@gmail.com>
2024-10-07 17:04:13 +0800

    fix edit modal

--- resources/views/admin/provinsi/edit-modal.blade.php
+++ resources/views/admin/provinsi/edit-modal.blade.php
@@ -1,24 +1,22 @@
 <!-- Edit Provinsi Modal -->
 <div id="editProvinsiModal" class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full hidden">
     <div class="relative inset-y-1/2 -translate-y-1/2 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
-        <div class="mt-3 text-center">
-            <form id="editProvinsiForm" action="#" method="POST">
-                @csrf
-                @method('PUT')
-                <h3 class="text-lg leading-6 font-medium text-gray-900 mb-3">Tambah Provinsi</h3>
-                <input type="text" id="editProvinsiName" name="nama"
-                    class="w-full px-3 py-2 mb-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    placeholder="Nama provinsi" required>
-                <span class="text-red-800">{{ $errors->first('nama') }}</span>
-                <hr class="h-1 my-3">
-                <div class="flex items-center">
-                    <button type="button" id="cancelEditProvinsi"
-                        class="px-4 py-2 bg-gray-300 text-black rounded-md hover:bg-gray-400 mr-2">Batalkan</button>
-                    <button type="submit" id="confirmEditProvinsi"
-                        class="flex-1 px-4 py-2 bg-[#3560A0] text-white rounded-md hover:bg-blue-700">Simpan</button>
-                </div>
-            </form>
-        </div>
+        <form id="editProvinsiForm" action="#" method="POST">
+            @csrf
+            @method('PUT')
+            <h3 class="text-lg leading-6 font-medium text-gray-900 mb-3">Edit Provinsi</h3>
+            <input type="text" id="editProvinsiName" name="nama"
+                class="w-full px-3 py-2 mb-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Nama provinsi" required>
+            <span class="text-red-800">{{ $errors->first('nama') }}</span>
+            <hr class="h-1 my-3">
+            <div class="flex items-center">
+                <button type="button" id="cancelEditProvinsi"
+                    class="px-4 py-2 bg-gray-300 text-black rounded-md hover:bg-gray-400 mr-2">Batalkan</button>
+                <button type="submit" id="confirmEditProvinsi"
+                    class="flex-1 px-4 py-2 bg-[#3560A0] text-white rounded-md hover:bg-blue-700">Simpan</button>
+            </div>
+        </form>
     </div>
 </div>
 
