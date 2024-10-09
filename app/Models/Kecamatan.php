4a484b5ddf933c7c92ad0166b8c35a48ae88a0e7
novilfahlevy <novilfreon@gmail.com>
2024-10-09 10:18:52 +0800

    mengubah field 'nama_kecamatan' menjadi 'nama' pada model Kecamatan

--- app/Models/Kecamatan.php
+++ app/Models/Kecamatan.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Kecamatan extends Model
 {
@@ -11,5 +12,24 @@ class Kecamatan extends Model
 
     protected $table = 'kecamatan';
 
-    protected $fillable = ['nama_kecamatan', 'kabupaten_id'];
+    protected $fillable = ['nama', 'kabupaten_id'];
+
+    public function getThreeDigitsId(): string {
+        $id = $this->getKey();
+        $digits = strlen($id);
+
+        if ($digits == 1) {
+            return "00$id";
+        }
+
+        if ($digits == 2) {
+            return "0$id";
+        }
+
+        return $id;
+    }
+
+    public function kabupaten(): BelongsTo {
+        return $this->belongsTo(Kabupaten::class, 'kabupaten_id');
+    }
 }
