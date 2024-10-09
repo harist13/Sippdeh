1d5ebb875dc70adb4c025fafa697c2c300aa7298
novilfahlevy <novilfreon@gmail.com>
2024-10-09 21:10:17 +0800

    hapus kelurahan

--- app/Http/Controllers/KelurahanController.php
+++ app/Http/Controllers/KelurahanController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreKelurahanRequest;
+use App\Http\Requests\UpdateKelurahanRequest;
 use App\Models\Kecamatan;
 use App\Models\Kelurahan;
 use Exception;
@@ -67,9 +68,20 @@ class KelurahanController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateKelurahanRequest $request, string $id)
     {
-        //
+        try {
+            $validated = $request->validated();
+
+            $kelurahan = Kelurahan::find($id);
+            $kelurahan->nama = $validated['nama'];
+            $kelurahan->kecamatan_id = $validated['kecamatan_id'];
+            $kelurahan->save();
+
+            return redirect()->back()->with('status_pengeditan_kelurahan', 'berhasil');
+        } catch (Exception $error) {
+            return redirect()->back()->with('status_pengeditan_kelurahan', 'gagal');
+        }
     }
 
     /**
@@ -77,6 +89,13 @@ class KelurahanController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            $kelurahan = Kelurahan::find($id);
+            $kelurahan->delete();
+
+            return redirect()->back()->with('status_penghapusan_kelurahan', 'berhasil');
+        } catch (Exception $error) {
+            return redirect()->back()->with('status_penghapusan_kelurahan', 'gagal');
+        }
     }
 }
