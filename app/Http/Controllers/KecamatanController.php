28997cb92c7f44a4ee69ab33ec077a0a0721859c
novilfahlevy <novilfreon@gmail.com>
2024-10-10 19:15:36 +0800

    cari kecamatan dan pilih kabupaten

--- app/Http/Controllers/KecamatanController.php
+++ app/Http/Controllers/KecamatanController.php
@@ -25,15 +25,24 @@ class KecamatanController extends Controller
 
             // kembalikan lagi ke halaman Daftar Kecamatan kalau query 'cari'-nya ternyata kosong.
             if ($kataKunci == '') {
+                // jika pengguna juga mencari kabupaten, maka tetap sertakan kabupaten di URL-nya.
+                if ($request->has('kabupaten')) {
+                    return redirect()->route('kecamatan', ['kabupaten' => $request->get('kabupaten')]);
+                }
+
                 return redirect()->route('kecamatan');
             }
 
             $kecamatanQuery->whereLike('nama', "%$kataKunci%");
         }
 
-        $kecamatan = $kecamatanQuery->orderByDesc('id')->paginate(10);
+        if ($request->has('kabupaten')) {
+            $kecamatanQuery->where('kabupaten_id', $request->get('kabupaten'));
+        }
 
-        return view('admin.kecamatan.index', compact('kecamatan', 'kabupaten'));
+        $kecamatan = $kecamatanQuery->orderByDesc('id')->paginate(10);
+        
+        return view('admin.kecamatan.index', compact('kabupaten', 'kecamatan'));
     }
 
     /**
