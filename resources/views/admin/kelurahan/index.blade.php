9b7e59e22fe224207010dfffb4369a7909b2c03c
novilfahlevy <novilfreon@gmail.com>
2024-10-10 21:23:34 +0800

    fix kelurahan

--- resources/views/admin/kelurahan/index.blade.php
+++ resources/views/admin/kelurahan/index.blade.php
@@ -75,39 +75,7 @@
                 <span class="text-lg font-bold"><i class="fas fa-city"></i> Kelurahan</span>
             </div>
             <div class="flex flex-col-mobile gap-5 space-y-2-mobile w-full-mobile">
-                <div class="relative w-[300px] w-full-mobile">
-                    <button id="dropdownButton" class="bg-gray-100 w-full border border-gray-300 text-gray-700 py-2 px-4 rounded-lg flex items-center justify-between w-full-mobile">
-                        Pilih Kab/Kota <i class="fas fa-chevron-right ml-2"></i>
-                    </button>
-                    <div id="dropdownMenu" class="absolute mt-2 inset-x-0 rounded-lg shadow-lg bg-white z-10 hidden">
-                        <ul class="py-1 text-gray-700">
-                            @if (request()->has('cari'))
-                                <a href="{{ route('kelurahan') }}?cari={{ request()->get('cari') }}">
-                                    <li class="px-4 py-2 hover:bg-gray-100">
-                                        Semua
-                                    </li>
-                                </a>
-                            @else
-                                <a href="{{ route('kelurahan') }}">
-                                    <li class="px-4 py-2 hover:bg-gray-100">
-                                        Semua
-                                    </li>
-                                </a>
-                            @endif
-                            @foreach ($kabupaten as $kab)
-                                @if (request()->has('cari'))
-                                    <a href="{{ route('kelurahan') }}?cari={{ request()->get('cari') }}&kabupaten={{ $kab->id }}">   
-                                        <li class="px-4 py-2 hover:bg-gray-100">{{ $kab->nama }}</li>
-                                    </a>
-                                @else
-                                    <a href="{{ route('kelurahan') }}?kabupaten={{ $kab->id }}">
-                                        <li class="px-4 py-2 hover:bg-gray-100">{{ $kab->nama }}</li>
-                                    </a>
-                                @endif
-                            @endforeach
-                        </ul>
-                    </div>
-                </div>
+                @include('components.dropdown-kabupaten', ['kabupaten' => $kabupaten, 'routeName' => 'kelurahan'])
 
                 <form action="{{ route('kelurahan') }}" method="GET">
                     <div class="flex items-center border border-gray-300 rounded-lg bg-gray-100 px-4 py-2">
