6c2e3fd5ba3cbb413f36616ddbb6462698869e10
novilfahlevy <novilfreon@gmail.com>
2024-10-15 20:45:35 +0800

    border dan judul

--- app/Exports/KecamatanExport.php
+++ app/Exports/KecamatanExport.php
@@ -2,11 +2,14 @@
 
 namespace App\Exports;
 
+use App\Models\Kabupaten;
 use App\Models\Kecamatan;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class KecamatanExport implements FromView {
+class KecamatanExport implements FromView, WithStyles {
   private $kabupatenId;
 
   public function __construct(int $kabupatenId) {
@@ -14,12 +17,44 @@ class KecamatanExport implements FromView {
   }
   
   public function view(): View {
-    if ($this->kabupatenId == 0) {
-        $kecamatan = Kecamatan::all();
-    } else {
-        $kecamatan = Kecamatan::whereKabupatenId($this->kabupatenId)->get();
+	$kabupaten = null;
+
+	if ($this->kabupatenId == 0) {
+		$kecamatan = Kecamatan::selectRaw('id, UPPER(nama) AS nama')->get();
+	} else {
+		$kabupaten = Kabupaten::selectRaw('id, UPPER(nama) AS nama')->find($this->kabupatenId);
+        $kecamatan = Kecamatan::selectRaw('id, UPPER(nama) AS nama')->whereKabupatenId($this->kabupatenId)->get();
     }
     
-    return view('exports.kecamatan', compact('kecamatan'));
+    return view('exports.kecamatan', compact('kabupaten', 'kecamatan'));
+  }
+
+  public function styles(Worksheet $sheet)
+  {
+	$styleArray = [
+		'borders' => [
+			'bottom' => ['borderStyle' => 'thin', 'color' => ['argb' => '00000000']],
+			'top' => ['borderStyle' => 'thin', 'color' => ['argb' => '00000000']],
+			'right' => ['borderStyle' => 'thin', 'color' => ['argb' => '00000000']],
+			'left' => ['borderStyle' => 'thin', 'color' => ['argb' => '00000000']],
+		],
+	];
+
+	$index = 1;
+
+	foreach ($sheet->getRowIterator() as $row) {
+		$namaKabupaten = $sheet->getCell('A1');
+		$namaKabupaten->getStyle()->getFont()->setBold(true);
+
+		if ($index > 1) {
+			$cellA = $sheet->getCell("A$index");
+			$cellA->getStyle()->applyFromArray($styleArray);
+
+			$cellB = $sheet->getCell("B$index");
+			$cellB->getStyle()->applyFromArray($styleArray);
+		}
+
+		$index++;
+	}
   }
 }
--- resources/views/exports/kecamatan.blade.php
+++ resources/views/exports/kecamatan.blade.php
@@ -1,15 +1,18 @@
-<table>
+<table style="border: 1px solid black;">
 	<thead>
 		<tr>
-			<th>Kecamatan</th>
-			<th>Kabupaten</th>
+			<th colspan="2" style="font-size: 16px;">{{ $kabupaten->nama }}</th>
+		</tr>
+		<tr>
+			<th width="300px" style="font-size: 16px;"><strong>ID</strong></th>
+			<th width="300px" style="font-size: 16px;"><strong>KECAMATAN</strong></th>
 		</tr>
 	</thead>
 	<tbody>
 		@foreach($kecamatan as $kec)
 			<tr>
-				<td>{{ $kec->nama }}</td>
-				<td>{{ $kec->kabupaten->nama }}</td>
+				<td style="font-size: 16px;">{{ $kec->getThreeDigitsId() }}</td>
+				<td style="font-size: 16px;">{{ $kec->nama }}</td>
 			</tr>
 		@endforeach
 	</tbody>
