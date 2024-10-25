625ccaea854b1c0cc3b048b2be0f3d6ca8a18f62
novilfahlevy <novilfreon@gmail.com>
2024-10-25 20:58:37 +0800

    memindahkan controller pilgub

--- app/Http/Controllers/Operator/PilgubController.php
+++ app/Http/Controllers/Operator/PilgubController.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Operator;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class PilgubController extends Controller
 {
-    //
     public function index()
     {
         $userWilayah = session('user_wilayah');
