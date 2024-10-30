9b209e6f39140b58af1fe1663754af90bb00b648
novilfahlevy <novilfreon@gmail.com>
2024-10-30 11:17:23 +0800

    menambah calon seeder

--- database/seeders/CalonSeeder.php
+++ database/seeders/CalonSeeder.php
@@ -13,6 +13,7 @@ class CalonSeeder extends Seeder
      */
     public function run(): void
     {
+        // Pilgub Kaltim
         Calon::create([
             'nama' => 'Isran Noor',
             'nama_wakil' => 'Hadi Mulyadi',
@@ -21,17 +22,209 @@ class CalonSeeder extends Seeder
         ]);
 
         Calon::create([
-            'nama' => "Rudi Mas'ud",
+            'nama' => "Rudy Mas'ud",
             'nama_wakil' => 'Seno Aji',
             'posisi' => 'GUBERNUR',
             'provinsi_id' => 23, // Kalimantan Timur
         ]);
 
+        // Pilkada Samarinda
         Calon::create([
             'nama' => 'Andi Harun',
             'nama_wakil' => 'Saefuddin Zuhri',
             'posisi' => 'WALIKOTA',
             'kabupaten_id' => 10, // Kota Samarinda
+        ]);
+
+        // Pilkada Balikpapan
+        Calon::create([
+            'nama' => 'Muhammad Sa’bani',
+            'nama_wakil' => 'Syukri Wahid',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 8, // Kota Balikpapan
+        ]);
+
+        Calon::create([
+            'nama' => 'Rendi Susiswo Ismail',
+            'nama_wakil' => 'Eddy Sunardi Darmawan',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 8, // Kota Balikpapan
+        ]);
+
+        Calon::create([
+            'nama' => 'Rahmad Mas’ud',
+            'nama_wakil' => 'Bagus Susetyo',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 8, // Kota Balikpapan
+        ]);
+
+        // Pilkada Kutai Kartanegara
+        Calon::create([
+            'nama' => 'Edi Damansyah',
+            'nama_wakil' => 'Rendi Solihin',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 3, // Kabupaten Kutai Kartanegara
+        ]);
+
+        Calon::create([
+            'nama' => 'Awang Yacoub Luthman',
+            'nama_wakil' => 'Akhmad Zais',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 3, // Kabupaten Kutai Kartanegara
+        ]);
+
+        Calon::create([
+            'nama' => 'Dendi Suryadi',
+            'nama_wakil' => 'Alif Turiadi',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 3, // Kabupaten Kutai Kartanegara
+        ]);
+
+        // Pilkada Kutai Barat
+        Calon::create([
+            'nama' => 'Sahadi',
+            'nama_wakil' => 'Alexander Edmond',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 2, // Kabupaten Kutai Barat
+        ]);
+
+        Calon::create([
+            'nama' => 'Frederick Edwin',
+            'nama_wakil' => 'Nanang Adriani',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 2, // Kabupaten Kutai Barat
+        ]);
+
+        Calon::create([
+            'nama' => 'Ahmad Syaiful',
+            'nama_wakil' => 'Jainudin',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 2, // Kabupaten Kutai Barat
+        ]);
+
+        // Pilkada Kutai Timur
+        Calon::create([
+            'nama' => 'Ardiansyah Sulaiman',
+            'nama_wakil' => 'Mahyunadi',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 4, // Kabupaten Kutai Timur
+        ]);
+
+        Calon::create([
+            'nama' => 'Kasmidi Bulang',
+            'nama_wakil' => 'Kinsu',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 4, // Kabupaten Kutai Timur
+        ]);
+
+        // Pilkada Bontang
+        Calon::create([
+            'nama' => 'Basri Rase',
+            'nama_wakil' => 'Chusnul Dhihin',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 9, // Kota Bontang
+        ]);
+
+        Calon::create([
+            'nama' => 'Neni Moerniaeni',
+            'nama_wakil' => 'Agus Haris',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 9, // Kota Bontang
+        ]);
+
+        Calon::create([
+            'nama' => 'Najirah',
+            'nama_wakil' => 'Muhammad Aswar',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 9, // Kota Bontang
+        ]);
+
+        Calon::create([
+            'nama' => 'Sutomo Jabir',
+            'nama_wakil' => 'Nasrullah',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 9, // Kota Bontang
+        ]);
+
+        // Pilkada Mahakam Ulu
+        Calon::create([
+            'nama' => 'Yohanes Avun',
+            'nama_wakil' => 'Y. Juan Jenau',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 5, // Kabupaten Mahakam Ulu
+        ]);
+
+        Calon::create([
+            'nama' => 'Owena Mayang Shari Belawan',
+            'nama_wakil' => 'Stanislaus Liah',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 5, // Kabupaten Mahakam Ulu
+        ]);
+
+        Calon::create([
+            'nama' => 'Novita Bulan',
+            'nama_wakil' => 'Artya Fathra Marthin',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 5, // Kabupaten Mahakam Ulu
+        ]);
+
+        // Pilkada Penajam Paser Utara
+        Calon::create([
+            'nama' => 'Mudyat Noor',
+            'nama_wakil' => 'Abdul Waris Muin',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 7, // Kabupaten Penajam Paser Utara
+        ]);
+
+        Calon::create([
+            'nama' => 'Desmon Hariman Sormin',
+            'nama_wakil' => 'Naspi Arsyad',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 7, // Kabupaten Penajam Paser Utara
+        ]);
+
+        Calon::create([
+            'nama' => 'Andi Harahap',
+            'nama_wakil' => 'Dayang Dona Walfaries Tania',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 7, // Kabupaten Penajam Paser Utara
+        ]);
+
+        Calon::create([
+            'nama' => 'Hamdam',
+            'nama_wakil' => 'Ahmad Basir',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 7, // Kabupaten Penajam Paser Utara
+        ]);
+
+        // Pilkada Paser
+        Calon::create([
+            'nama' => 'Fahmi Fadli',
+            'nama_wakil' => 'Ikhwan Antasari',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 6, // Kabupaten Paser
+        ]);
+
+        Calon::create([
+            'nama' => 'Syarifah Masitah Assegaf',
+            'nama_wakil' => 'Denni Mappa',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 6, // Kabupaten Paser
+        ]);
+
+        // Pilkada Berau
+        Calon::create([
+            'nama' => 'Sri Juniarsih Mas',
+            'nama_wakil' => 'Gamalis',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 1, // Kabupaten Berau
+        ]);
+
+        Calon::create([
+            'nama' => 'Madri Pani',
+            'nama_wakil' => 'Agus Wahyudi',
+            'posisi' => 'WALIKOTA',
+            'kabupaten_id' => 1, // Kabupaten Berau
         ]);
     }
 }
