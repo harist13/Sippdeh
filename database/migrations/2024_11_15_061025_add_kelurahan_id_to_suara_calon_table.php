63705e363a2c97f421cc824d24821587872df9de
novilfahlevy <novilfreon@gmail.com>
2024-11-15 15:54:35 +0800

    dropback migrations

--- database/migrations/2024_11_15_061025_add_kelurahan_id_to_suara_calon_table.php
+++ database/migrations/2024_11_15_061025_add_kelurahan_id_to_suara_calon_table.php
@@ -11,17 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('suara_calon', function (Blueprint $table) {
-            // Add new foreign key and column changes
-            $table->foreignId('kelurahan_id')
-                ->nullable()
-                ->constrained('kelurahan')
-                ->nullOnDelete();
+        // Schema::table('suara_calon', function (Blueprint $table) {
+        //     // Add new foreign key and column changes
+        //     $table->foreignId('kelurahan_id')
+        //         ->nullable()
+        //         ->constrained('kelurahan')
+        //         ->nullOnDelete();
 
-            // Drop the old foreign key for tps_id and column
-            $table->dropForeign(['tps_id']);
-            $table->dropColumn('tps_id');
-        });
+        //     // Drop the old foreign key for tps_id and column
+        //     $table->dropForeign(['tps_id']);
+        //     $table->dropColumn('tps_id');
+        // });
     }
 
     /**
@@ -29,16 +29,16 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('suara_calon', function (Blueprint $table) {
-            // Remove the kelurahan_id column and foreign key
-            $table->dropForeign(['kelurahan_id']);
-            $table->dropColumn('kelurahan_id');
+        // Schema::table('suara_calon', function (Blueprint $table) {
+        //     // Remove the kelurahan_id column and foreign key
+        //     $table->dropForeign(['kelurahan_id']);
+        //     $table->dropColumn('kelurahan_id');
             
-            // Re-add the tps_id column and foreign key
-            $table->foreignId('tps_id')
-                ->nullable()
-                ->constrained('tps')
-                ->nullOnDelete();
-        });
+        //     // Re-add the tps_id column and foreign key
+        //     $table->foreignId('tps_id')
+        //         ->nullable()
+        //         ->constrained('tps')
+        //         ->nullOnDelete();
+        // });
     }
 };
