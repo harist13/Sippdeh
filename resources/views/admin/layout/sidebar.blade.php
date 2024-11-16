3464808b50b3440af6fee8dbe9b71b8511c35a51
harist13 <razak1352.com@gmail.com>
2024-11-16 19:18:25 +0800

    hapus menu suara

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -35,15 +35,6 @@
             </svg>
             <span class="ml-2">Resume</span>
         </a>
-        <a href="{{ route('suara') }}"
-            class="sidebar-item flex items-center px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-200 rounded-lg mx-2 {{ Request::routeIs('suara') ? 'active' : '' }}">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd"
-                    d="M3.46447 3.46447C2 4.92893 2 7.28595 2 12C2 16.714 2 19.0711 3.46447 20.5355C4.92893 22 7.28595 22 12 22C16.714 22 19.0711 22 20.5355 20.5355C22 19.0711 22 16.714 22 12C22 7.28595 22 4.92893 20.5355 3.46447C19.0711 2 16.714 2 12 2C7.28595 2 4.92893 2 3.46447 3.46447ZM17 8.25C17.4142 8.25 17.75 8.58579 17.75 9V18C17.75 18.4142 17.4142 18.75 17 18.75C16.5858 18.75 16.25 18.4142 16.25 18V9C16.25 8.58579 16.5858 8.25 17 8.25ZM12.75 12C12.75 11.5858 12.4142 11.25 12 11.25C11.5858 11.25 11.25 11.5858 11.25 12V18C11.25 18.4142 11.5858 18.75 12 18.75C12.4142 18.75 12.75 18.4142 12.75 18V12ZM7 14.25C7.41421 14.25 7.75 14.5858 7.75 15V18C7.75 18.4142 7.41421 18.75 7 18.75C6.58579 18.75 6.25 18.4142 6.25 18V15C6.25 14.5858 6.58579 14.25 7 14.25Z"
-                    fill="currentColor" />
-            </svg>
-            <span class="ml-2">Suara</span>
-        </a>
 
         <hr class="my-2 ml-2 mr-2 rounded-md gradient-hr">
 
@@ -62,23 +53,23 @@
         <div class="px-2 py-2 text-gray-700 font-bold text-base">Data</div>
         <a href="{{ route('provinsi')}}"
             class="sidebar-item flex items-center px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-200 rounded-lg mx-2 {{ Request::routeIs('provinsi') ? 'active' : '' }}">
-            <img src="{{ asset('assets/icon/provinsi.svg') }}" class="ml-3" alt="Provinsi">
-            <span class="ml-3">Provinsi</span>
+            <img src="{{ asset('assets/icon/provinsi.svg') }}" alt="Provinsi">
+            <span class="ml-2">Provinsi</span>
         </a>
         <a href="{{ route('kabupaten')}}"
             class="sidebar-item flex items-center px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-200 rounded-lg mx-2 {{ Request::routeIs('kabupaten') ? 'active' : '' }}">
-            <img src="{{ asset('assets/icon/kabupaten.svg') }}" class="ml-3" alt="Kabupaten">
-            <span class="ml-3">Kabupaten/Kota</span>
+            <img src="{{ asset('assets/icon/kabupaten.svg') }}" alt="Kabupaten">
+            <span class="ml-2">Kabupaten/Kota</span>
         </a>
         <a href="{{ route('kecamatan')}}"
             class="sidebar-item flex items-center px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-200 rounded-lg mx-2 {{ Request::routeIs('kecamatan') ? 'active' : '' }}">
-            <img src="{{ asset('assets/icon/kecamatan.svg') }}" class="ml-3" alt="Kecamatan">
-            <span class="ml-3">Kecamatan</span>
+            <img src="{{ asset('assets/icon/kecamatan.svg') }}" alt="Kecamatan">
+            <span class="ml-2">Kecamatan</span>
         </a>
         <a href="{{ route('kelurahan')}}"
             class="sidebar-item flex items-center px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-200 rounded-lg mx-2 {{ Request::routeIs('kelurahan') ? 'active' : '' }}">
-            <img src="{{ asset('assets/icon/kelurahan.svg') }}" class="ml-3" alt="Kelurahan">
-            <span class="ml-3">Kelurahan</span>
+            <img src="{{ asset('assets/icon/kelurahan.svg') }}" alt="Kelurahan">
+            <span class="ml-2">Kelurahan</span>
         </a>
         <a href="{{ route('tps')}}"
             class="sidebar-item flex items-center px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-200 rounded-lg mx-2 {{ Request::routeIs('tps') ? 'active' : '' }}">
