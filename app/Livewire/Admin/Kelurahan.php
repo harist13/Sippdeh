3b13f2406a94194b4171f14e52f43165c71b5707
novilfahlevy <novilfreon@gmail.com>
2024-11-16 17:07:55 +0800

    menggunakan livewire untuk daftar kelurahan

---
+++ app/Livewire/Admin/Kelurahan.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use App\Models\Kecamatan;
+use App\Models\Kelurahan as Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Livewire\Component;
+use Livewire\WithoutUrlPagination;
+use Livewire\WithPagination;
+
+class Kelurahan extends Component
+{
+    use WithPagination, WithoutUrlPagination;
+    
+    public int $perPage = 10;
+
+    public string $keyword = '';
+    
+    public function render()
+    {
+        $kecamatan = Kecamatan::all();
+        $kelurahan = $this->getKelurahan();
+        return view('admin.kelurahan.livewire', compact('kecamatan', 'kelurahan'));
+    }
+
+    private function getKelurahan(): LengthAwarePaginator
+    {
+        $kelurahanQuery = Model::query();
+
+        if ($this->keyword) {
+            $keyword = '%' . strtolower($this->keyword) . '%';
+            $kelurahanQuery
+                ->whereRaw('LOWER(nama) LIKE ?', [$keyword])
+                ->orWhereHas('kecamatan', fn (Builder $builder) => $builder->whereRaw('LOWER(nama) LIKE ?', [$keyword]));
+        }
+
+        return $kelurahanQuery->paginate($this->perPage);
+    }
+}
