f1d3890ef7dab244e9abd527f00388776d1c3d58
novilfahlevy <novilfreon@gmail.com>
2024-11-19 21:42:56 +0800

    rekap pilgub

--- app/Livewire/Operator/Dashboard/DiagramBarPilgub.php
+++ app/Livewire/Operator/Dashboard/DiagramBarPilgub.php
@@ -16,6 +16,18 @@ use Illuminate\Support\Collection;
 #[Lazy]
 class DiagramBarPilgub extends Component
 {
+    public function placeholder()
+    {
+        return <<<'HTML'
+            <div class="flex justify-center my-20 w-[1080px]">
+                <svg class="animate-spin h-8 w-8 text-[#3560A0]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+            </div>
+        HTML;
+    }
+
     public function render()
     {
         $data = $this->getData();
