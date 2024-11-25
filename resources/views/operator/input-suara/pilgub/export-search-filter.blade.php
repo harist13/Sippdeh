1de096e5255d687cafb22a64e5591f9d2b38dfa1
novilfahlevy <novilfreon@gmail.com>
2024-11-25 08:22:53 +0800

    disable tombol export di input suara

--- resources/views/operator/input-suara/pilgub/export-search-filter.blade.php
+++ resources/views/operator/input-suara/pilgub/export-search-filter.blade.php
@@ -1,18 +1,15 @@
 <div class="flex flex-col space-y-2 sm:space-y-0 sm:space-x-2 sm:flex-row sm:items-center order-1 lg:order-2">
     {{-- Export Button --}}
-    <button wire:click="export" wire:loading.attr="disabled" wire:target="export" class="flex items-center bg-[#EE3C46] disabled:bg-[#EE3C406c] text-white text-sm font-medium px-4 py-2 rounded-lg sm:w-auto w-full">
-        {{-- Export Icon --}}
+    {{-- <button wire:click="export" wire:loading.attr="disabled" wire:target="export" class="flex items-center bg-[#EE3C46] disabled:bg-[#EE3C406c] text-white text-sm font-medium px-4 py-2 rounded-lg sm:w-auto w-full">
         <i wire:loading.remove wire:target="export" class="fas fa-file-export w-4 h-4 mr-2"></i>
 
-        {{-- Loading Icon --}}
         <svg wire:loading wire:target="export" class="animate-spin -ml-1 mr-2 h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
             <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
             <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
         </svg>
 
-        {{-- Label --}}
         <span>Ekspor</span>
-    </button>
+    </button> --}}
     
     {{-- Search Input --}}
     <div class="flex items-center rounded-lg bg-[#ECEFF5] px-4 py-2">
