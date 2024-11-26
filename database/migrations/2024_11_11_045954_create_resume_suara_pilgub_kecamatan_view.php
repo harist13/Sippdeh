01691e20db3ed47fec84ce3f5cd39ec5d1ea3870
novilfahlevy <novilfreon@gmail.com>
2024-11-26 10:01:26 +0800

    .

--- database/migrations/2024_11_11_045954_create_resume_suara_pilgub_kecamatan_view.php
+++ database/migrations/2024_11_11_045954_create_resume_suara_pilgub_kecamatan_view.php
@@ -17,15 +17,66 @@ return new class extends Migration
                 kecamatan.id AS id,
                 kecamatan.nama AS nama,
                 kecamatan.kabupaten_id AS kabupaten_id,
-                COALESCE(SUM(resume_suara_pilgub_tps.dpt), 0) AS dpt,
-                COALESCE(SUM(resume_suara_pilgub_tps.kotak_kosong), 0) AS kotak_kosong,
-                COALESCE(SUM(resume_suara_pilgub_tps.suara_sah), 0) AS suara_sah,
-                COALESCE(SUM(resume_suara_pilgub_tps.suara_tidak_sah), 0) AS suara_tidak_sah,
-                COALESCE(SUM(resume_suara_pilgub_tps.suara_masuk), 0) AS suara_masuk,
-                COALESCE(SUM(resume_suara_pilgub_tps.abstain), 0) AS abstain,
+
+                -- Kotak Kosong
+                COALESCE(SUM(resume_suara_pilgub_kelurahan.dpt), 0) AS dpt,
+                (
+                    COALESCE(SUM(resume_suara_pilgub_kelurahan.kotak_kosong), 0)
+                    +
+                    COALESCE(SUM(daftar_pemilih_pilgub_kecamatan_view.kotak_kosong), 0)
+                ) AS kotak_kosong,
+
+                -- Suara Sah
+                (
+                    COALESCE(SUM(resume_suara_pilgub_kelurahan.suara_sah), 0)
+                    +
+                    COALESCE(SUM(daftar_pemilih_pilgub_kecamatan_view.suara_sah), 0)
+                ) AS suara_sah,
+
+                -- Suara Tidak Sah
+                (
+                    COALESCE(SUM(resume_suara_pilgub_kelurahan.suara_tidak_sah), 0)
+                    +
+                    COALESCE(SUM(daftar_pemilih_pilgub_kecamatan_view.suara_tidak_sah), 0)
+                ) AS suara_tidak_sah,
+
+                -- Suara Masuk
+                (
+                    COALESCE(SUM(resume_suara_pilgub_kelurahan.suara_masuk), 0)
+                    +
+                    COALESCE(SUM(daftar_pemilih_pilgub_kecamatan_view.suara_masuk), 0)
+                ) AS suara_masuk,
+                
+                COALESCE(SUM(resume_suara_pilgub_kelurahan.abstain), 0) AS abstain,
+
                 CASE 
-                    WHEN COALESCE(SUM(resume_suara_pilgub_tps.dpt), 0) > 0 
-                    THEN ROUND(((COALESCE(SUM(resume_suara_pilgub_tps.suara_sah), 0) + COALESCE(SUM(resume_suara_pilgub_tps.suara_tidak_sah), 0)) / COALESCE(SUM(resume_suara_pilgub_tps.dpt), 0)) * 100, 1)
+                    WHEN COALESCE(SUM(resume_suara_pilgub_kelurahan.dpt), 0) > 0 
+                    THEN ROUND(
+                        (
+                            (
+                                -- (Suara Sah + Suara Tidah Sah) / (DPT + DPTb + DPK)
+                                (
+                                    COALESCE(SUM(resume_suara_pilgub_kelurahan.suara_sah), 0)
+                                    +
+                                    COALESCE(SUM(daftar_pemilih_pilgub_kecamatan_view.suara_sah), 0)
+                                )
+                                +
+                               (
+                                    COALESCE(SUM(resume_suara_pilgub_kelurahan.suara_tidak_sah), 0)
+                                    +
+                                    COALESCE(SUM(daftar_pemilih_pilgub_kecamatan_view.suara_tidak_sah), 0)
+                                )
+                            )
+                            /
+                            (
+                                COALESCE(SUM(resume_suara_pilgub_kelurahan.dpt), 0)
+                                +
+                                COALESCE(SUM(daftar_pemilih_pilgub_kecamatan_view.dptb), 0)
+                                +
+                                COALESCE(SUM(daftar_pemilih_pilgub_kecamatan_view.dpk), 0)
+                            )
+                        ) * 100, 1
+                    )
                     ELSE 0 
                 END AS partisipasi
             FROM 
@@ -33,11 +84,9 @@ return new class extends Migration
             LEFT JOIN 
                 kelurahan ON kelurahan.kecamatan_id = kecamatan.id
             LEFT JOIN 
-                tps ON tps.kelurahan_id = kelurahan.id
+                resume_suara_pilgub_kelurahan ON resume_suara_pilgub_kelurahan.id = kelurahan.id
             LEFT JOIN 
-                resume_suara_pilgub_tps ON resume_suara_pilgub_tps.id = tps.id
-            LEFT JOIN 
-                daftar_pemilih ON daftar_pemilih.kecamatan_id = kecamatan.id AND daftar_pemilih.posisi = 'GUBERNUR'
+                daftar_pemilih_pilgub_kecamatan_view ON daftar_pemilih_pilgub_kecamatan_view.id = kecamatan.id
             GROUP BY 
                 kecamatan.id;
         ");
