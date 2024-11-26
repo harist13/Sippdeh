0fc47f2354477b3fe4c0eaabb02ce6f6f94c9083
harist13 <razak1352.com@gmail.com>
2024-11-26 21:58:08 +0800

    add kop di masing masing wilayah

--- resources/views/exports/resume-suara-pilwali-wilayah-pdf.blade.php
+++ resources/views/exports/resume-suara-pilwali-wilayah-pdf.blade.php
@@ -22,38 +22,48 @@
             margin-bottom: 30px;
             border-bottom: 2px solid black;
             padding-bottom: 20px;
+            width: 100%;
         }
         .logo-container {
             position: absolute;
-            left: 0;
-            top: 0;
+            left: 30px;
+            top: 10px;
         }
         .logo {
-            width: 80px;
+            width: 150px;
             height: auto;
         }
         .header-text {
-            margin-left: 90px;
             text-align: center;
+            margin: 0 auto;
+            padding: 10px 0;
         }
         .header-text h2 {
-            font-size: 14pt;
+            font-size: 16pt;
             font-weight: bold;
             margin: 0;
             padding: 5px 0;
             text-transform: uppercase;
         }
         .header-text h3 {
-            font-size: 12pt;
+            font-size: 14pt;
             font-weight: bold;
             margin: 0;
             padding: 5px 0;
             text-transform: uppercase;
         }
         .header-text p {
-            font-size: 9pt;
+            font-size: 10pt;
             margin: 0;
             padding: 2px 0;
+            line-height: 1.3;
+        }
+        .website-email {
+            margin-top: 5px;
+        }
+        .website-email a {
+            color: black;
+            text-decoration: none;
         }
         .text-center { text-align: center; }
         .text-right { text-align: right; }
@@ -82,19 +92,111 @@
     </style>
 </head>
 <body>
+    @php
+        $kabupatenName = session('operator_kabupaten_name');
+        $jenisWilayah = session('operator_jenis_wilayah');
+        $kabupatenId = session('operator_kabupaten_id');
+    @endphp
+
+    @if(str_contains(strtolower($kabupatenName), 'bontang'))
     <div class="letterhead">
         <div class="logo-container">
-            @if($logo && file_exists(public_path('storage/' . $logo)))
-                <img src="{{ public_path('storage/' . $logo) }}" class="logo">
-            @endif
+            <img src="{{ public_path('storage/' . $logo) }}" class="logo">
         </div>
         <div class="header-text">
-            <h2>PEMERINTAH {{ strtoupper($kabupaten->nama ?? '') }}</h2>
+            <h2>PEMERINTAH KOTA BONTANG</h2>
             <h3>BADAN KESATUAN BANGSA DAN POLITIK</h3>
-            <p>{{ $kabupaten->alamat ?? '' }}</p>
-            <p>Telepon: {{ $kabupaten->telepon ?? '-' }} - Email: {{ $kabupaten->email ?? '-' }}</p>
+            <p>Jl. Bessai Berinta No.82, Tanjung Laut, Kec. Bontang Sel.</p>
+            <p>Kota Bontang, Kalimantan Timur 75325</p>
+            <p class="website-email">Telp: (0548) 21307 Email: kesbangpol@bontangkota.go.id</p>
         </div>
     </div>
+
+    @elseif(str_contains(strtolower($kabupatenName), 'kutai kartanegara'))
+    <div class="letterhead">
+        <div class="logo-container">
+            <img src="{{ public_path('storage/' . $logo) }}" class="logo">
+        </div>
+        <div class="header-text">
+            <h2>PEMERINTAH KABUPATEN KUTAI KARTANEGARA</h2>
+            <h3>BADAN KESATUAN BANGSA DAN POLITIK</h3>
+            <p>Komplek Perkantoran Gedung Bukit Pelangi</p>
+            <p>Jl. Wolter Monginsidi Tenggarong 75513</p>
+            <p class="website-email">Telp. (0541) 661004 Fax. (0541) 662608</p>
+        </div>
+    </div>
+
+    @elseif(str_contains(strtolower($kabupatenName), 'kutai barat'))
+    <div class="letterhead">
+        <div class="logo-container">
+            <img src="{{ public_path('storage/' . $logo) }}" class="logo">
+        </div>
+        <div class="header-text">
+            <h2>PEMERINTAH KABUPATEN KUTAI BARAT</h2>
+            <h3>BADAN KESATUAN BANGSA DAN POLITIK</h3>
+            <p>Jl. Sendawar Raya Barong Tongkok</p>
+            <p>Kutai Barat, Kalimantan Timur 75776</p>
+            <p class="website-email">Telp/Fax. (0545) 4041415</p>
+        </div>
+    </div>
+
+    @elseif(str_contains(strtolower($kabupatenName), 'samarinda'))
+    <div class="letterhead">
+        <div class="logo-container">
+            <img src="{{ public_path('storage/' . $logo) }}" class="logo">
+        </div>
+        <div class="header-text">
+            <h2>PEMERINTAH KOTA SAMARINDA</h2>
+            <h3>BADAN KESATUAN BANGSA DAN POLITIK</h3>
+            <p>Alamat Jalan Balaikota No. 27 Telepon (0541) 733033 Fax (0541) 741429</p>
+            <p>Samarinda (Kalimantan Timur) Kode Pos 75121</p>
+            <p class="website-email">
+                <a href="http://kesbangpol.samarindakota.go.id/">http://kesbangpol.samarindakota.go.id/</a>
+                Email: kesbangpol.samarindakota@gmail.com
+            </p>
+        </div>
+    </div>
+
+    @elseif(str_contains(strtolower($kabupatenName), 'kutai timur'))
+    <div class="letterhead">
+        <div class="logo-container">
+            <img src="{{ public_path('storage/' . $logo) }}" class="logo">
+        </div>
+        <div class="header-text">
+            <h2>PEMERINTAH KABUPATEN KUTAI TIMUR</h2>
+            <h3>BADAN KESATUAN BANGSA DAN POLITIK</h3>
+            <p>Jl. Soekarno-Hatta, Sangatta Utara</p>
+            <p>Kutai Timur, Kalimantan Timur 75683</p>
+            <p class="website-email">Telp. (0549) 21006 Fax. (0549) 21002</p>
+        </div>
+    </div>
+
+    @elseif(str_contains(strtolower($kabupatenName), 'paser'))
+    <div class="letterhead">
+        <div class="logo-container">
+            <img src="{{ public_path('storage/' . $logo) }}" class="logo">
+        </div>
+        <div class="header-text">
+            <h2>PEMERINTAH KABUPATEN PASER</h2>
+            <h3>BADAN KESATUAN BANGSA DAN POLITIK</h3>
+            <p>Kompleks Perkantoran Terpadu</p>
+            <p>Jl. Kesuma Bangsa Km.05, Tanah Grogot 76211</p>
+            <p class="website-email">Telp. (0543) 21002 Fax. (0543) 21002</p>
+        </div>
+    </div>
+
+    @else
+    <div class="letterhead">
+        <div class="logo-container">
+            <img src="{{ public_path('storage/' . $logo) }}" class="logo">
+        </div>
+        <div class="header-text">
+            <h2>PEMERINTAH {{ $jenisWilayah === 'kota' ? 'KOTA' : 'KABUPATEN' }} {{ strtoupper($kabupatenName) }}</h2>
+            <h3>BADAN KESATUAN BANGSA DAN POLITIK</h3>
+            <p>{{ $kabupatenName }}, Kalimantan Timur</p>
+        </div>
+    </div>
+    @endif
 
     @php
         $totalDpt = $data->sum('dpt');
@@ -212,7 +314,7 @@
                     
                     @if(in_array('KELURAHAN', $includedColumns))
                         <td class="text-left">
-                            @if($isTpsView)
+                           @if($isTpsView)
                                 {{ $item->tps?->kelurahan?->nama ?? '-' }}
                             @else
                                 {{ $item->nama }}
