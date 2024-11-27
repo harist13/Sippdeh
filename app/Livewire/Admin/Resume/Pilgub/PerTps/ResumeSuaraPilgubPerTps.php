0779e7baf756920ce9d13be6ff74e9200b946afb
novilfahlevy <novilfreon@gmail.com>
2024-11-27 21:56:13 +0800

    fix partisipasi admin resume pilgub per tps

--- app/Livewire/Admin/Resume/Pilgub/PerTps/ResumeSuaraPilgubPerTps.php
+++ app/Livewire/Admin/Resume/Pilgub/PerTps/ResumeSuaraPilgubPerTps.php
@@ -44,8 +44,8 @@ class ResumeSuaraPilgubPerTps extends Component
 
     public array $selectedKecamatan = [];
     public array $selectedKelurahan = [];
-    public array $includedColumns = ['KABUPATEN', 'KECAMATAN', 'KELURAHAN', 'TPS', 'CALON'];
-    public array $partisipasi = ['HIJAU', 'KUNING', 'MERAH'];
+    public array $includedColumns = ['KABUPATEN/KOTA', 'KECAMATAN', 'KELURAHAN', 'TPS', 'CALON'];
+    public array $partisipasi = ['HIJAU', 'MERAH'];
 
     public function mount($kabupatenId = null)
     {
@@ -162,15 +162,11 @@ class ResumeSuaraPilgubPerTps extends Component
         try {
             $builder->where(function (Builder $builder) {
                 if (in_array('MERAH', $this->partisipasi)) {
-                    $builder->orWhereRaw('partisipasi BETWEEN 0 AND 59.9');
-                }
-            
-                if (in_array('KUNING', $this->partisipasi)) {
-                    $builder->orWhereRaw('partisipasi BETWEEN 60 AND 79.9');
+                    $builder->orWhereRaw('partisipasi < 77.5');
                 }
                 
                 if (in_array('HIJAU', $this->partisipasi)) {
-                    $builder->orWhereRaw('partisipasi >= 80');
+                    $builder->orWhereRaw('partisipasi >= 77.5');
                 }
             });
         } catch (Exception $exception) {
@@ -226,10 +222,10 @@ class ResumeSuaraPilgubPerTps extends Component
     #[On('reset-filter')] 
     public function resetFilter(): void
     {
-        $this->includedColumns = ['KABUPATEN', 'KECAMATAN', 'KELURAHAN', 'TPS', 'CALON'];
+        $this->includedColumns = ['KABUPATEN/KOTA', 'KECAMATAN', 'KELURAHAN', 'TPS', 'CALON'];
         $this->selectedKecamatan = [];
         $this->selectedKelurahan = [];
-        $this->partisipasi = ['HIJAU', 'KUNING', 'MERAH'];
+        $this->partisipasi = ['HIJAU', 'MERAH'];
     }
 
     #[On('apply-filter')]
@@ -250,7 +246,16 @@ class ResumeSuaraPilgubPerTps extends Component
                 $this->selectedKelurahan,
                 $this->includedColumns,
                 $this->partisipasi,
-                $this->kabupatenId
+    
+                $this->dptSort,
+                $this->suaraSahSort,
+                $this->suaraTidakSahSort,
+                $this->suaraMasukSort,
+                $this->abstainSort,
+                $this->partisipasiSort,
+    
+                $this->paslonIdSort,
+                $this->paslonSort,
             );
     
             return Excel::download($sheet, 'resume-suara-pemilihan-gubernur.xlsx');
